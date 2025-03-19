b676e3a7e01d5596e07d1a18a1c400e166b69e0a
Youcef <youcefdermeche@gmail.com>
2025-03-19 14:38:12 +0100

    description

---
+++ Helloworld.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# This file produce some printing
+
+print("Hello world")
+
+
+# We introduce some interaction with the user
+
+name = input("What's your name?")
+print("\nHello " + name)
+
